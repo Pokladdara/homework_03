076a148d06e7a43aee3651d0bd7c2a5f8d5cc299
Pokladdara <pokladdara@gmail.com>
2025-04-23 18:19:17 +0300

    Update homework_03_03.py

--- homework_03_03.py
+++ homework_03_03.py
@@ -7,7 +7,6 @@ phone_numbers = ["    +38(050)123-32-34",
                  "38050 111 22 11   "
                  ]
 def normalize_phone(phone_numbers):
-    normalized = [] # Список для зберігання нормалізованих номерів 
     for phone in phone_numbers:
         cleaned_number = re.sub(r'[^0-9]' , ''  , phone) #Прибираемо всі символи окрім цифр 
         if not cleaned_number.startswith('+'): # Перевіряемо починается номер з +
@@ -15,6 +14,6 @@ def normalize_phone(phone_numbers):
                 cleaned_number = '+' + cleaned_number
             else:
                  cleaned_number = '+38' + cleaned_number # Додаємо +38 до номерів в які починаются з 0
-            normalized.append(cleaned_number) # Додавання до списку з нормалізованими номерами 
-    return normalized
-print("Нормалізовані номери телефонів для SMS-розсилки:", normalize_phone(phone_numbers)) #Виклик функції
+                 return cleaned_number
+           
+print("Нормалізований номер телефону для SMS-розсилки:", normalize_phone(phone_numbers)) #Виклик функції
